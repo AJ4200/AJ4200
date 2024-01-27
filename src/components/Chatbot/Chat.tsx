eb65848ef262003c84aa888bde054e65bafc19a8
Abel Majadibodu <Abeljackson33@gmail.com>
2024-01-27 15:23:31 +0200

    Added some chat features

--- src/components/Chatbot/Chat.tsx
+++ src/components/Chatbot/Chat.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import Draggable from "react-draggable";
+import { FiSend, FiX } from "react-icons/fi";
+import { FaGripLines, FaRobot, FaUser } from "react-icons/fa";
 import FloatingBot from "./FloatingBot";
+import AccessibilityToggle from "./AccessibilityToggle";
 import { getTextColor } from "@/lib/navbarUtils";
 import { useRouter } from "next/router";
-import { FiSend, FiX } from "react-icons/fi";
-import AccessibilityToggle from "./AccessibilityToggle";
-import  Draggable  from "react-draggable";
 
 interface ChatProps {
   onClose: () => void;
@@ -43,75 +44,93 @@ const Chat: React.FC<ChatProps> = ({ onClose }) => {
     return responses[Math.floor(Math.random() * responses.length)];
   };
 
-    return (
-        <Draggable
-        >
-    <div className="fixed bottom-4 right-4 rounded border-[2px] border-[var(--neon)] p-4 shadow backdrop-blur-md">
-      <FiX
-        className="absolute right-2 top-2 border border-[var(--neon)] p-1 text-2xl text-[var(--neon)]"
-        onClick={onClose}
-      />
-      <div className="my-2 flex justify-center space-x-2 ">
-        <FloatingBot onClick={onClose} />
-        <h1
+  return (
+    <Draggable>
+      <div className="fixed bottom-4 right-4 rounded border-[2px] border-[var(--neon)] p-4 shadow backdrop-blur-xl">
+        <FaGripLines
           className={
-            "text-shadow-theme text-4xl font-bold" + ` ${getTextColor(asPath)}`
+            "absolute left-[50%] right-[50%] top-0 p-1 text-3xl transition-transform hover:scale-[1.25] hover:text-[var(--neon)] " +
+            `${getTextColor(asPath)}`
           }
-        >
-          NootBot<span className="text-sm font-thin">alpha</span>
-        </h1>
-      </div>
-
-      {messages.length > 0 ? (
-        <div className="mb-4 h-96 overflow-y-auto">
-          {messages.map((message, index) => (
-            <div key={index} className="max-w-80">
-              {message.type === "user" ? (
-                <p className="text-darkshadow whitespace-normal break-words text-right text-white">
-                  {message.content}
-                </p>
-              ) : (
-                <p
-                  className={`${getTextColor(
-                    asPath,
-                  )} whitespace-normal break-words shadow-2xl backdrop-blur-3xl`}
-                >
-                  {message.content}
-                </p>
-              )}
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="text-shadow-theme my-2">
-          <h2 className="u font-semibold ">
-            how may i assist you?
-          </h2>
-          <span className="flex items-center justify-center space-x-2 border border-[var(--neon)]  ">
-            <AccessibilityToggle route={asPath} />
-            Font Accessibility
-          </span>
-        </div>
-      )}
-
-      <div className="flex items-center">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Type your message..."
-          className="text-outline flex-grow rounded-l bg-[var(--neon)] p-2 placeholder:text-black focus:outline-none"
         />
-        <button
-          onClick={handleSendMessage}
-          className="rounded-r border border-[var(--neon)] p-2 text-xl text-[var(--neon)] backdrop-blur-lg hover:bg-[var(--neon)] focus:outline-none"
-        >
-          <FiSend />
-        </button>
-      </div>
-    </div>
-        </Draggable>
+        <FiX
+          className="absolute right-0 top-0 rounded-tr border border-[var(--neon)] p-1 text-2xl text-[var(--neon)]"
+          onClick={onClose}
+        />
+        <div className="my-4 flex justify-center space-x-2 ">
+          <FloatingBot onClick={onClose} />
+          <h1
+            className={
+              "text-shadow-theme text-4xl font-bold" +
+              ` ${getTextColor(asPath)}`
+            }
+          >
+            NootBot<span className="text-sm font-thin">alpha</span>
+          </h1>
+        </div>
 
+        {messages.length > 0 ? (
+          <div className="mb-4 h-96 space-y-2 overflow-y-auto">
+            {messages.map((message, index) => (
+              <div key={index} className="max-w-80">
+                {message.type === "user" ? (
+                  <div className="flex items-center justify-end space-x-2">
+                    <p className="text-darkshadow max-w-[calc(100%-3rem)] whitespace-normal break-words border border-[var(--neon)] p-1 text-white">
+                      {message.content}
+                    </p>
+                    <FaUser
+                      className={
+                        "rounded-full border border-[var(--neon)] text-4xl " +
+                        `${getTextColor(asPath)}`
+                      }
+                    />
+                  </div>
+                ) : (
+                  <div className="flex items-center space-x-2">
+                    <FaRobot
+                      className={
+                        "rounded-full border border-[var(--neon)] text-4xl " +
+                        `${getTextColor(asPath)}`
+                      }
+                    />
+                    <p
+                      className={`${getTextColor(
+                        asPath,
+                      )} max-w-[calc(100%-3rem)] whitespace-normal break-words shadow-2xl backdrop-blur-3xl`}
+                    >
+                      {message.content}
+                    </p>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-shadow-theme my-2">
+            <h2 className="u font-semibold ">how may i assist you?</h2>
+            <span className="flex items-center justify-center space-x-2 border border-[var(--neon)]">
+              <AccessibilityToggle route={asPath} />
+              Font Accessibility
+            </span>
+          </div>
+        )}
+
+        <div className="flex items-center">
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type your message..."
+            className="text-outline flex-grow rounded-l bg-[var(--neon)] p-2 placeholder:text-black focus:outline-none"
+          />
+          <button
+            onClick={handleSendMessage}
+            className="rounded-r border border-[var(--neon)] p-2 text-xl text-[var(--neon)] backdrop-blur-lg hover:bg-[var(--neon)] focus:outline-none"
+          >
+            <FiSend />
+          </button>
+        </div>
+      </div>
+    </Draggable>
   );
 };
 
